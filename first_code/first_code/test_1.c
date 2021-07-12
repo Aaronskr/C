cc0230db7b7a0978513a2051ff3f23311aef9360
wanyuxi <1520130180@qq.com>
2021-07-12 16:11:37 +0800

    初始C语言1

---
+++ first_code/first_code/test_1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int num = 0;
+	scanf("%d", &num);
+	printf("%d\n", num);
+
+	printf("hello bit\n");
+
+
+	printf("hehe\n");
+	printf("haha\n");
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	return 0;
+}
+
+
+
